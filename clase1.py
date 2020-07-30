df4ed78d573b737f791a48cdf8d71dc206d57407
Oscar Oswaldo Meza Leon <oscar.meza.leon@gmail.com>
2020-07-30 13:33:20 -0600

    Implementacion del resto de metodos, mejorando busqueda en lista
    Implemente el resto de metodos de la aplicacion de consola.
    Estoy intentando arreglar un metodo para buscar un elemento en una lista y que devuelva un booleano para dejarlo en uin solo return.

--- clase1.py
+++ clase1.py
@@ -1,18 +1,49 @@
-clients= 'leon,sofia'
+clients= 'leon,sofia,'
 
 def create_client(client_name):
     global clients
-    __add_comma()
-    clients+= client_name
- 
+    if check_client(client_name):
+        clients+= client_name
+        __add_comma()
+        return True
+    else:
+        print("ERROR")
+        return False
+
+
+def check_client(client_name):
+    global clients
+    return ~(client_name in clients)
 
 
 def __add_comma():
     global clients
-
     clients +=','
 
-
-if __name__== '__main__':
-    create_client('Scarm')
+def list_clients():
+    global clients
     print(clients)
+
+def __print_welcome():
+    print('Welcome to leon ventas')
+    print('*' * 50)
+    print('What would you like to do today? ')
+    print('1. Create client')
+    print('2. Delete Client')
+    print('3. Print all the clients')
+if __name__== '__main__':
+    __print_welcome()
+
+    command = input()
+
+    if command == '1':
+        client_name = input('What its the client name? ')
+        if create_client(client_name):
+            print('The client has been added succesfully')
+            list_clients()
+    elif command == '2':
+        pass
+    elif command == '3':
+         list_clients()
+    else:
+        print('invalid command')
