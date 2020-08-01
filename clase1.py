915d1d9e5d7efbac2a9154f0f59e698ca4454436
Oscar Oswaldo Meza Leon <oscar.meza.leon@gmail.com>
2020-07-31 19:13:59 -0600

    Crud parcialmente terminado
    Termine la practica del crud de consola, no pude avanzar mas en el dia por un error con la plataforma.
    Espero implementar un loop para el command y hacer el codigo mas agradable.

--- clase1.py
+++ clase1.py
@@ -21,20 +21,20 @@ def _get_client_name():
 def client_update(client_name,updated_client_name):
     global clients
 
-    if updated_client_name not in clients:
-        if client_name in clients:
-            clients = clients.replace(client_name + ',', updated_client_name)
-        else:
-            print('The client that you are trying to update doesnt exist')
+    #if updated_client_name not in clients:
+    if client_name in clients:
+        clients = clients.replace(client_name, updated_client_name)
     else:
-            print('The client that you are trying to update already exist')
+        print('The client that you are trying to update doesnt exist')
+    #else:
+      #      print('The client that you are trying to update already exist')
 
 def __add_comma():
     global clients
     clients +=','
 
 
-def clients_list():
+def read_client_list():
     global clients
     print(clients)
 
@@ -45,9 +45,9 @@ def __print_welcome():
     print('*' * 50)
     print('What would you like to do today? ')
     print('1. Create client')
-    print('2. Delete Client')
-    print('4. Update client ')
-    print('5. Print all the clients')
+    print('2. Read the list')
+    print('3. Update Client')
+    print('4. Delete client ')
 
 if __name__== '__main__':
     __print_welcome()
@@ -58,16 +58,16 @@ if __name__== '__main__':
         client_name = _get_client_name()
         if client_create(client_name):
             print('The client has been added succesfully')
-            clients_list()
+            read_client_list()
     elif command == '2':
-        delete_client(_get_client_name())
-        clients_list()
-    elif command == '4':
+        read_client_list()  
+    elif command == '3':
         client_name = _get_client_name()
         client_name_update = _get_client_name()
         client_update(client_name,client_name_update)
-        clients_list()
-    elif command == '5':
-         clients_list()    
+        read_client_list()  
+    elif command == '4':     
+        delete_client(_get_client_name())
+        read_client_list()
     else:
         print('invalid command')
